fc3f94eee398073ed705b0c8f1dba89b671d779d
Allen <allenchen0220@gmail.com>
2016-10-16 13:38:10 +0800

    20 練習 Entity Framework 發生驗證例外時的處裡方法

--- MVC5Course/Controllers/EFController.cs
+++ MVC5Course/Controllers/EFController.cs
@@ -1,6 +1,7 @@
 ﻿using MVC5Course.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -66,7 +67,22 @@ namespace MVC5Course.Controllers
         {
             var product = db.Product.Find(id);
             product.ProductName += "!";
-            db.SaveChanges();
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                foreach(var entityErrors in ex.EntityValidationErrors)
+                {
+                    foreach (var vErrors in entityErrors.ValidationErrors)
+                    {
+                        throw new DbEntityValidationException(vErrors.PropertyName + " 發生錯誤:" + vErrors.ErrorMessage);
+                    }
+                }
+
+            }
 
             return RedirectToAction("Index");
         }
