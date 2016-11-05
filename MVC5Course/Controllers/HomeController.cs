bff788f2778f01e8135050db678ef5b8f7acdf45
Allen <allenchen0220@gmail.com>
2016-11-05 10:43:17 +0800

    57 練習 @Ajax.ActionLink 的使用方式

--- MVC5Course/Controllers/HomeController.cs
+++ MVC5Course/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
@@ -12,12 +13,18 @@ namespace MVC5Course.Controllers
     {
         public ActionResult Index()
         {
+            //Session["aa"] = 1;
+            //Thread.Sleep(10000);
             return View();
         }
 
+        public ActionResult GetTime()
+        {
+            return Content(DateTime.Now.ToString());
+        }
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page.";// + Session["aa"];
 
             return View();
         }
@@ -50,4 +57,5 @@ namespace MVC5Course.Controllers
             return View();
         }
     }
+
 }
